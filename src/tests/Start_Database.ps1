c7a0e07b228c422f33e7c8acb777a5c456e3ee75
Richard-s-2020 <richard.spreij.minecraftspelen@gmail.com>
2025-01-04 22:25:19 +0100

    fase2

--- src/tests/Start_Database.ps1
+++ src/tests/Start_Database.ps1
@@ -9,21 +9,37 @@ if (!(($jsonFilePath -eq "") -or ($null -eq $jsonFilePath))) {
         $Text = [PSCustomObject]@{}
         $Color = [PSCustomObject]@{}
         $install = [PSCustomObject]@{}
+        $air = [PSCustomObject]@{}
 
         $jsonContent.PSObject.Properties | ForEach-Object {
             $_.value.PSObject.Properties | ForEach-Object {
                 if ($_.Name -eq "Categories") {
                     $Categories = $_.Value
+                    $Text | Add-Member -MemberType NoteProperty -Name $Categories -Value $air
+                    $air = [PSCustomObject]@{}
+                    $Color | Add-Member -MemberType NoteProperty -Name $Categories -Value $air
+                    $air = [PSCustomObject]@{}
+                    $install | Add-Member -MemberType NoteProperty -Name $Categories -Value $air
                 }
                 elseif ($_.Name -eq "text") {
                     $_.Value.PSObject.Properties | ForEach-Object {
-                        $Temp_text = $_.name       
+                        $Temp_text = $_.name      
                         $_.value.PSObject.Properties | ForEach-Object {
-                            if ($_.Name -eq "Title"){
-
+                            
+                            if ($_.Name -eq "Title") {
+                                $temp_Name = $_.value
+                                $Text.$Categories | Add-Member -MemberType NoteProperty -Name $Temp_text -Value $temp_Name
+                            }
+                            else {
+                                $Temp_Color = $_.value
+                                $Color.$Categories | Add-Member -MemberType NoteProperty -Name $Temp_text -Value $Temp_Color
                             }
                         }
-                        Pause
+                    }
+                }
+                else {
+                    $_.Value.PSObject.Properties | Sort-Object | ForEach-Object {
+                        $_.value
                     }
                 }
             }
